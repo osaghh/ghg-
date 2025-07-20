e61c37df10053607ec8f5706be397be7c88bf8b1
osaghh <osaggh@mail.ru>
2025-07-21 05:07:15 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold mb-6">Instagram Downloader</h1>
+    </main>
+  );
+}
+
+export default App;
